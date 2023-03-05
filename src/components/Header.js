89d632f6e7419c2bc6f370d9c4b8b8d797323262
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-05 21:27:43 +0100

    Implemented About page, Footer page and Linking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button
-        color={showAddTask ? 'red' : 'green'}
-        text={showAddTask ? 'Close' : 'Add'}
-        onClick={onAdd}
-      />
+      {location.pathname === '/' && (
+        <Button
+          color={showAddTask ? 'red' : 'green'}
+          text={showAddTask ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
