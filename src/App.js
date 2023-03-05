e6e3583c67c00c77e73feaa130fe9b8520ee2736
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-05 20:16:37 +0100

    Implemented Add/Close Task toggle

--- src/App.js
+++ src/App.js
@@ -51,7 +51,10 @@ function App() {
 
   return (
     <div className='container'>
-      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAddTask={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
